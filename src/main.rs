84452fe2320511d71a422c5ce84216ead104cb70
Henry Stanley <henry@henrystanley.com>
2018-06-25 12:43:03 -0400

    make a blinking thing

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,51 @@ extern crate pxl;
 
 use pxl::*;
 
-fn main() {
-    pxl::run::<Conway>();
+struct Conway {
+    frame_num: u32,
 }
 
-struct Conway {}
+impl Conway {
+    fn get_color(&self) -> (f32, f32, f32) {
+        let frame = self.frame_num as f32;
+        let frame_step = 1.0 / 60.0;
+        let red = frame * frame_step;
+        return (red, 0.0, 0.0);
+    }
+
+    fn tick_frame(&mut self) {
+        if self.frame_num > 60 {
+            self.frame_num = 1;
+        }
+
+        self.frame_num += 1;
+    }
+}
 
 impl Program for Conway {
-    fn new() -> Self {
-        return Conway{};
+    fn new() -> Conway {
+        Conway { 
+            frame_num: 0
+        }
     }
 
     fn dimensions() -> (usize, usize) {
         return (700, 500);
     }
+
+    fn render(&mut self, _pixels: &mut [Pixel]) {
+        for pixel in _pixels.iter_mut() {
+            let colors = self.get_color();
+
+            pixel.red = colors.0;
+            pixel.blue = colors.1;
+            pixel.green = colors.2;
+        }
+        self.tick_frame();
+        self.frame_num += 1;
+    }
+}
+
+fn main() {
+    pxl::run::<Conway>();
 }
